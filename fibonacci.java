df3fb776a871135708fd0980f0d02a5ae5bbcfde
codyssey-sampurnasen <sampurnasen333@gmail.com>
2026-04-24 21:47:31 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, n3;
+        for(int i=0; i<5; i++) {
+            System.out.print(n1 + " ");
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
